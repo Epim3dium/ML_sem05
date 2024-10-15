f9f4eb38484aa781b955434f596248d059cc82bb
Epim3dium <Epim3dium@protonmail.com>
2024-10-15 20:34:12 +0100

    4 way coloring for the last plot

--- assignment1/ex1.py
+++ assignment1/ex1.py
@@ -14,14 +14,10 @@ b = np.random.multivariate_normal(mean, cov , 500). T
 c = np.concatenate((a, b) , axis=1 )
 c = c.T
 
-idtab = ['a'] * 500 + ['b'] * 500
-df = pd.DataFrame(c)
-df.insert(2, 'id', idtab, True)
 np.random.shuffle(c)
 c = c.T
 x = c[0]
 y = c[1]
-id = c[2]
 
 n = 1000
 index1 = np.random.randint(0, n - 1)
@@ -40,7 +36,7 @@ rx2 = x[index2]
 ry2 = y[index2]
 
 def pp1_3(rx1, ry1, rx2, ry2, x, y):
-    iter_count = 100
+    iter_count = 1000
     for iter in range(iter_count):
         print(f"before r1: {rx1}, {ry1}");
         print(f"before r2: {rx2}, {ry2}");
@@ -85,12 +81,71 @@ for iter in range(iter_count):
     print(f"after r1: {rx1}, {ry1}");
     print(f"after r2: {rx2}, {ry2}");
 
-plt.plot( x , y , 'x' )
+r1cluster = [[], []]
+r2cluster = [[], []]
+for i in range(1000):
+    if distance(x[i], y[i], rx1, ry1) < distance(x[i], y[i], rx2, ry2):
+        r1cluster[0].append(x[i])
+        r1cluster[1].append(y[i])
+    else:
+        r2cluster[0].append(x[i])
+        r2cluster[1].append(y[i])
+# plt.plot( x , y , 'x' )
 plt.plot( a[0] , a[1] , 'x', color='g')
 plt.plot( b[0] , b[1] , 'x', color='c')
+plt.xlim(-8, 8)
+plt.ylim(-8, 8)
+plt.show()
 
+
+for line in plt.gca().lines:
+    line.remove()
+plt.plot( r1cluster[0] , r1cluster[1] , 'x', color='g')
+plt.plot( r2cluster[0] , r2cluster[1] , 'x', color='c')
 plt.plot(rx1path, ry1path, 'o', color='r') 
 plt.plot(rx2path, ry2path, 'o', color='b') 
 plt.plot([rx1, rx2], [ry1, ry2], 'o', color='y') 
-plt.axis('equal')
+plt.xlim(-8, 8)
+plt.ylim(-8, 8)
+plt.show()
+
+r1A = [[], []]
+r1B = [[], []]
+r2A = [[], []]
+r2B = [[], []]
+for i in range(1000):
+    a_contains = -1 
+    for ii in range(len(a[0])):
+        if x[i] == a[0][ii] and y[i] == a[1][ii]:
+            a_contains = ii
+            break
+    r1_contains = -1
+    for ii in range(len(r1cluster[0])):
+        if x[i] == r1cluster[0][ii] and y[i] == r1cluster[1][ii]:
+            r1_contains = ii
+            break
+    if a_contains != -1:
+        if r1_contains != -1:
+            r1A[0].append(x[i])
+            r1A[1].append(y[i])
+        else:
+            r2A[0].append(x[i])
+            r2A[1].append(y[i])
+    else:
+        if r1_contains != -1:
+            r1B[0].append(x[i])
+            r1B[1].append(y[i])
+        else:
+            r2B[0].append(x[i])
+            r2B[1].append(y[i])
+
+for line in plt.gca().lines:
+    line.remove()
+
+plt.plot( r1A[0] , r1A[1] , 'x', color='g')
+plt.plot( r1B[0] , r1B[1] , 'x', color='r')
+plt.plot( r2A[0] , r2A[1] , 'x', color='b')
+plt.plot( r2B[0] , r2B[1] , 'x', color='c')
+plt.xlim(-8, 8)
+plt.ylim(-8, 8)
 plt.show()
