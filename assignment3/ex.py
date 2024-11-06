59a450996790b86a49c42204ba7bcc2e9cfbebb0
Epim3dium <Epim3dium@protonmail.com>
2024-11-06 17:18:43 +0000

    avg & stddev

--- assignment3/ex.py
+++ assignment3/ex.py
@@ -1,4 +1,6 @@
 import random;
+import numpy
+
 UP    = 0
 DOWN  = 1
 LEFT  = 2
@@ -7,7 +9,7 @@ RIGHT = 3
 SIZE = 10
 
 def stateTransition(state, action):
-    if action == UP and sate > SIZE:
+    if action == UP and state > SIZE:
         state = state - SIZE
     if action == DOWN and state <= SIZE * (SIZE - 1):
         state = state + SIZE
@@ -22,7 +24,25 @@ def reward(state):
     return 0;
 def randAction():
     return [UP, DOWN, LEFT, RIGHT][random.randrange(4)]
-print(randAction())
+def episode(n):
+    state = 0
+    for i in range(n):
+        state = stateTransition(state, randAction())
+        if(reward(state) > 0):
+            return {"reward" : reward(state), "epochs" : i}
+    return {"reward" : 0, "epochs" : n}
+def simulation(k):
+    epochs = []
+    rewards = []
+    for _ in range(k):
+        e = episode(1000)
+        epochs.append(e["epochs"])
+        rewards.append(e["reward"])
+    print("iter averag: ", sum(epochs) / len(epochs))
+    print("iter stddev: ", numpy.std(epochs))
 
+    print("reward averag: ", sum(rewards) / len(rewards))
+    print("reward stddev: ", numpy.std(rewards))
+simulation(30)
 
 
